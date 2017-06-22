0b44289bcfef4e0f147d57f25648599dec8bd35e
dinofungus <dinofungus@hotmail.com>
2017-06-22 10:27:14 +1000

    Added flagging and mass clear

--- BasicMinesweeper/Form1.cs
+++ BasicMinesweeper/Form1.cs
@@ -18,7 +18,10 @@ namespace BasicMinesweeper
         //Arrays for cell data
         bool[] isBomb = new bool[81];
         bool[] uncovered = new bool[81];
+        bool[] flagged = new bool[81];
         int[] guideValue = new int[81];
+
+        int mines = 0;
 
         //Random number generator
         Random random = new Random();
@@ -31,7 +34,7 @@ namespace BasicMinesweeper
             gameArea.Left = 20;
             gameArea.Height = 270;
             gameArea.Width = 270;
-            gameArea.Click += gameArea_Click;
+            gameArea.MouseClick += gameArea_Click;
 
             InitializeComponent();
             gameArea.Show();
@@ -41,6 +44,8 @@ namespace BasicMinesweeper
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+            lblMines.Text = ("Mines: " + mines + "/10");
+
             //Get graphics for gameArea
             Graphics gr = gameArea.CreateGraphics();
 
@@ -54,7 +59,14 @@ namespace BasicMinesweeper
                 int x = (i % 9) * 30;
                 int y = (i / 9) * 30;
 
-                if (!uncovered[i])
+                if (flagged[i])
+                {
+                    Brush br = new SolidBrush(Color.Yellow);
+                    Rectangle rect = new Rectangle(x, y, 30, 30);
+                    gr.FillRectangle(br, rect);
+                    br.Dispose();
+                }
+                else if (!uncovered[i])
                 {
                     //Draw a grey box if cell is covered
                     Brush br = new SolidBrush(Color.DarkSlateGray);
@@ -75,7 +87,7 @@ namespace BasicMinesweeper
                         text = guideValue[i].ToString();
                     }
                     Brush br = new SolidBrush(Color.Black);
-                    Font font = new Font("Arial", 24);
+                    Font font = new Font("Arial", 23);
 
                     gr.DrawString(text, font, br, x, y);
                     br.Dispose();
@@ -93,7 +105,9 @@ namespace BasicMinesweeper
             {
                 isBomb[i] = false;
                 uncovered[i] = false;
+                flagged[i] = false;
                 guideValue[i] = 0;
+                mines = 0;
             }
 
             //Assign 10 bombs
@@ -182,7 +196,7 @@ namespace BasicMinesweeper
             Reset();
         }
 
-        public void gameArea_Click(object sender, EventArgs e)
+        public void gameArea_Click(object sender, MouseEventArgs e)
         {
             //Get a cursor position and convert to cell data
             Point pos = gameArea.PointToClient(Cursor.Position);
@@ -190,28 +204,43 @@ namespace BasicMinesweeper
             int posY = pos.Y / 30;
             int cell = (posY * 9) + posX;
 
-            //Make sure the mouse is clicked inside the game area
-            if (cell < 0 || cell > 81)
+            if (e.Button == MouseButtons.Right)
             {
-                return;
+                if (!uncovered[cell])
+                {
+                    flagged[cell] = true;
+                    mines++;
+                }
             }
-            //Tell the player and reset the game if they lose
-            else if (isBomb[cell])
+            else if(e.Button == MouseButtons.Left)
             {
-                MessageBox.Show("You lose");
-                Reset();
+                //Make sure the mouse is clicked inside the game area
+                if (cell < 0 || cell > 81)
+                {
+                    return;
+                }
+                //Tell the player and reset the game if they lose
+                else if (isBomb[cell] && !flagged[cell])
+                {
+                    uncovered[cell] = true;
+                    Form1_Paint(this, null);
+                    MessageBox.Show("You lose");
+                    Reset();
+                }
+                //otherwise uncover the clicked cell
+                else if (!flagged[cell])
+                {
+                    Uncover(cell);
+                }
+                //If all non-bomb cells are uncovered the player wins
+                if (CheckWin())
+                {
+                    MessageBox.Show("You win");
+                    Reset();
+                }
             }
-            //otherwise uncover the clicked cell
-            else
-            {                
-                uncovered[cell] = true;
-            }
-            //If all non-bomb cells are uncovered the player wins
-            if (CheckWin())
-            {
-                MessageBox.Show("You win");
-                Reset();
-            }
+
+            
 
             Form1_Paint(this, null);
 
@@ -221,7 +250,7 @@ namespace BasicMinesweeper
         public bool CheckWin()
         {
             //make sure each cell is either uncovered or a bomb
-            for (int i = 1; i < uncovered.Length; i++)
+            for (int i = 0; i < uncovered.Length; i++)
             {
                 if (!uncovered[i] && !isBomb[i])
                 {
@@ -229,6 +258,26 @@ namespace BasicMinesweeper
                 }
             }
             return true;
+        }
+
+        public void Uncover(int cell)
+        {
+            uncovered[cell] = true;
+            //If the cell is a 0, uncover all adjacent cells automatically
+            if (guideValue[cell] == 0)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    int modifier = (i / 3 - 1) * 9 + (i % 3 - 1);
+                    int value = cell + modifier;
+                    int rowDiff = Math.Abs(cell % 9 - value % 9);
+                    if (value >= 0 && value < 81 && rowDiff < 2 && !uncovered[value])
+                    {
+                        Uncover(cell + modifier);
+                    }
+                }
+            }
+            
         }
     }
 }
